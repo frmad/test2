67f8c446c24f93ba8eec61a34342faf4a32503a1
frmad <frmad21@student.sdu.dk>
2024-07-08 14:16:55 +0200

    setup gh-pages

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,6 +4,7 @@ function Home() {
     return (
         <div>
             <h2>Text</h2>
+            <p>Home</p>
         </div>
     );
 }
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -3,6 +3,7 @@ function Projects() {
     return (
         <div>
             <h2>Text</h2>
+            <p>Projects</p>
         </div>
     );
 }
