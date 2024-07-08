b33a22e64c34a3905f3f0fbb3d095f463ddae7b2
frmad <frmad21@student.sdu.dk>
2024-07-08 13:58:22 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route} from "react-router-dom";
 import Navbar from './Navbar';
 import About from './routes/About';
 import Projects from './routes/Projects';
 import Home from './routes/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-        <Navbar />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-        </Routes>
-    </div>
-  );
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
